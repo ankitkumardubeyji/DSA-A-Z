862ac885bf73de63a2f6eaf8e2cec63fd1714505
Ankit kumar Dubey <122801994+ankitkumardubeyji@users.noreply.github.com>
2024-03-15 11:56:45 +0000

    added Prime java file not running submiited cpp file

---
+++ Basic Maths/Prime.cpp
@@ -0,0 +1,15 @@
+#include<math.h>
+bool isPrime(int num)
+{
+	if(num==1){
+		return false;
+	}
+	// Write your code here.
+		for(int i=2;i*i<=num;i++){
+			if(num%i==0){
+				return false;
+			}
+		}
+		return true;
+	}
+
